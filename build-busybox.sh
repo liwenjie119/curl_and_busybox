3a3de49df5753269b4c472a56973709466119284
liwenjie119 <liwenjie119@126.com>
2019-12-15 15:31:56 +0800

    wget in silence

--- build-busybox.sh
+++ build-busybox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp .config busybox/.config
-wget http://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
+wget -b -c http://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 tar -xvJf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 export CROSS_COMPILE=$PWD/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 cd busybox
--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export MIN_SDK_VERSION=21
 
 # Set up Android NDK
 echo "Fetching Android NDK $NDK"
-[ -f "android-ndk-$NDK-$HOST_TAG.zip" ] || wget https://dl.google.com/android/repository/android-ndk-$NDK-$HOST_TAG.zip
+[ -f "android-ndk-$NDK-$HOST_TAG.zip" ] || wget -b -c https://dl.google.com/android/repository/android-ndk-$NDK-$HOST_TAG.zip
 [ -d "android-ndk-$NDK" ] || unzip -qo android-ndk-$NDK-$HOST_TAG.zip
 
 if [ -f /proc/cpuinfo ]; then
