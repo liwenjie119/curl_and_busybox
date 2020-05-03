60cb2a795fa7c5b9d183bee588897ccac1c0a0b2
liwenjie119 <liwenjie119@126.com>
2020-05-03 18:47:54 +0800

    update busybox

--- update.sh
+++ update.sh
@@ -3,14 +3,18 @@ git pull
 cd zlib
 git checkout develop
 git pull
+git checkout .
 cd ../busybox
 git checkout master
 git pull
+git checkout .
 cd ../openssl
 git checkout master
 git pull
+git checkout .
 cd ../curl
 git checkout master
 git pull
+git checkout .
 cd ..
 
